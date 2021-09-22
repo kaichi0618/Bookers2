895880a2fc7ff9ebe4b09e3d004aab98b440065a
EC2 Default User <ec2-user@ip-172-31-86-237.ec2.internal>
2021-09-22 14:17:20 +0000

    [finish]booker2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fb8b6ea7285695905891d71ef8cb1e5dc88302240aa1f5e55a46d65a65dc4091c284054286f54f399503a94257bceed343b5e25d45143d56a07f18c8336e59da'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'homes#top'
+  get '/home/about', to: 'homes#about'
+  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
+
 end
